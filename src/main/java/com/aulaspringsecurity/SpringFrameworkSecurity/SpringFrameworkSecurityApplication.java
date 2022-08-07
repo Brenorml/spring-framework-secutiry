a43ba926b586525bf7091d7d7a149ca1f6a2c4e8
Brenorml <breno.lucena@gmail.com>
2022-08-07 11:14:13 +0100

    Adicionado projeto spring sobre segurança

---
+++ src/main/java/com/aulaspringsecurity/SpringFrameworkSecurity/SpringFrameworkSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.aulaspringsecurity.SpringFrameworkSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aulaspringsecurity/SpringFrameworkSecurity/SpringFrameworkSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aulaspringsecurity.SpringFrameworkSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
